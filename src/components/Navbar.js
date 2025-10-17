0808b2a63b7356e1f0a0b5ca3dcd75501177326c
Saloni403 <156641454+Saloni403@users.noreply.github.com>
2025-10-17 16:04:55 +0530

    updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,19 +2,37 @@ import React from "react";
 import "./Navbar.css"
 import {Link} from 'react-router-dom'
 const Navbar = () => {
+  const openMenu = ()=>{
+    document.querySelector(".menu").classList.toggle("scrollmenu")
+  };
   return (
-    <div>
-    <header className="container d-flex align-items-center justify-content-between py-md-3">
-     <Link to="/"><img src="../rooftoplogo.webp" width="80"/></Link> 
+   <>
+    <header>
+      <div className="container-md px-md-3 px-0 d-flex align-items-center justify-content-between py-md-3">
+     <Link to="/"><img src="../rooftoplogo.webp" width="80" className="ms-md-0 ms-3"/></Link> 
       <nav className="d-md-block d-none">
         <Link to="/about">About</Link>
         <Link to="/contact">Contact</Link>
         <Link to="/offmarket">OffMarket</Link>
         <Link to="/onmarket">OnMarket</Link>
       </nav>
-      <i className="bi bi-list d-md-none "></i>
+      <i className="bi bi-list d-md-none" onClick={openMenu}></i>
+      </div>
     </header>
+
+    {/* menu */}
+  
+    <div className="menu py-5 px-3 d-md-none">
+      <a href="/"><img src="/images/logo-footer.webp" width="100"/></a> 
+    <ul>
+      <li><a href="/about">About</a></li>
+      <li><a href="/contact">Contact</a></li>
+      <li><a href="/offmarket">OffMarket</a></li>
+      <li><a href="/onmarket">OnMarket</a></li>
+    </ul>
     </div>
+   
+   </>
   );
 };
 
